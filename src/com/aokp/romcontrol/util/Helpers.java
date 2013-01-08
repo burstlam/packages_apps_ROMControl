2f0060ece49369d3d64b70620f655180b601258f
burstlam <burstlam@gmail.com>
2013-01-08 16:46:53 +0800

    Add Screen size helpers
    Change-Id: Icf0533d7e4772e866b6c5dc4ca45b429e75393e0

--- src/com/aokp/romcontrol/util/Helpers.java
+++ src/com/aokp/romcontrol/util/Helpers.java
@@ -9,6 +9,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
+import android.util.DisplayMetrics;
+import android.view.DisplayInfo;
+import android.view.WindowManager;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -22,6 +25,14 @@ import android.widget.Toast;
 import com.aokp.romcontrol.util.CMDProcessor.CommandResult;
 
 public class Helpers {
+
+    // Device types
+    private static final int DEVICE_PHONE = 0;
+    private static final int DEVICE_HYBRID = 1;
+    private static final int DEVICE_TABLET = 2;
+
+    // Device type reference
+    private static int mDeviceType = -1;
 
     private static final String TAG = "Helpers";
 
@@ -170,6 +181,11 @@ public class Helpers {
         return null;
     }
 
+
+    public static boolean fileExists(String filename) {
+        return new File(filename).exists();
+    }
+
     public static boolean writeOneLine(String fname, String value) {
         try {
             FileWriter fw = new FileWriter(fname);
@@ -312,5 +328,38 @@ public class Helpers {
         } else {
             return null;
         }
+    }
+
+    private static int getScreenType(Context con) {
+        if (mDeviceType == -1) {
+            WindowManager wm = (WindowManager)con.getSystemService(Context.WINDOW_SERVICE);
+            DisplayInfo outDisplayInfo = new DisplayInfo();
+            wm.getDefaultDisplay().getDisplayInfo(outDisplayInfo);
+            int shortSize = Math.min(outDisplayInfo.logicalHeight, outDisplayInfo.logicalWidth);
+            int shortSizeDp = shortSize * DisplayMetrics.DENSITY_DEFAULT / outDisplayInfo.logicalDensityDpi;
+            if (shortSizeDp < 600) {
+                // 0-599dp: "phone" UI with a separate status & navigation bar
+                mDeviceType =  DEVICE_PHONE;
+            } else if (shortSizeDp < 720) {
+                // 600-719dp: "phone" UI with modifications for larger screens
+                mDeviceType = DEVICE_HYBRID;
+            } else {
+                // 720dp: "tablet" UI with a single combined status & navigation bar
+                mDeviceType = DEVICE_TABLET;
+            }
+        }
+        return mDeviceType;
+    }
+
+    public static boolean isPhone(Context con) {
+        return getScreenType(con) == DEVICE_PHONE;
+    }
+
+    public static boolean isHybrid(Context con) {
+        return getScreenType(con) == DEVICE_HYBRID;
+    }
+
+    public static boolean isTablet(Context con) {
+        return getScreenType(con) == DEVICE_TABLET;
     }
 }
