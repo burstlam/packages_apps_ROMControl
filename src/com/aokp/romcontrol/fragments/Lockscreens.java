666c308ce25ad47c153a622c6d9edcb37ee9830a
burstlam <burstlam@gmail.com>
2013-01-08 16:27:13 +0800

    LockScreen Background Style (2/2)
    Change-Id: I8fc24b1e7d6bb8d1f525c02eeab352079aeaedea

--- src/com/aokp/romcontrol/fragments/Lockscreens.java
+++ src/com/aokp/romcontrol/fragments/Lockscreens.java
@@ -3,10 +3,9 @@ package com.aokp.romcontrol.fragments;
 import com.aokp.romcontrol.AOKPPreferenceFragment;
 import com.aokp.romcontrol.R;
 
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
-
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.app.FragmentTransaction;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -58,16 +57,24 @@ import java.io.InputStream;
 import java.util.ArrayList;
 
 import com.aokp.romcontrol.AOKPPreferenceFragment;
+import com.aokp.romcontrol.widgets.SeekBarPreference;
+import com.aokp.romcontrol.util.Helpers;
 import com.aokp.romcontrol.R;
 import com.aokp.romcontrol.ROMControlActivity;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.File;
+import java.io.IOException;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import net.margaritov.preference.colorpicker.ColorPickerView;
 
 public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String TAG = "Lockscreens";
     private static final boolean DEBUG = true;
+    private static final int LOCKSCREEN_BACKGROUND = 1024;
 
     private static final String PREF_VOLUME_ROCKER_WAKE = "volume_rocker_wake";
     private static final String PREF_VOLUME_MUSIC = "volume_music_controls";
@@ -79,6 +86,9 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
     private static final String PREF_LOCKSCREEN_TEXT_COLOR = "lockscreen_text_color";
     private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String PREF_LOCKSCREEN_HIDE_INITIAL_PAGE_HINTS = "lockscreen_hide_initial_page_hints";
+    private static final String KEY_ALWAYS_BATTERY_PREF = "lockscreen_battery_status";
+    private static final String KEY_BACKGROUND_PREF = "lockscreen_background";
+    private static final String KEY_BACKGROUND_ALPHA_PREF = "lockscreen_alpha";
 
     public static final int REQUEST_PICK_WALLPAPER = 199;
     public static final int REQUEST_PICK_CUSTOM_ICON = 200;
@@ -106,6 +116,16 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
     CheckBoxPreference mVibratePref;
     CheckBoxPreference mLockscreenHideInitialPageHints;
 
+    ListPreference mBatteryStatus;
+    ListPreference mCustomBackground;
+    SeekBarPreference mBgAlpha;
+
+    private boolean mIsScreenLarge;
+    private Activity mActivity;
+    private ContentResolver mResolver;
+    private File wallpaperImage;
+    private File wallpaperTemporary;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -113,6 +133,10 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.prefs_lockscreens);
+
+        mActivity = getActivity();
+        mResolver = mActivity.getContentResolver();
+        mIsScreenLarge = Helpers.isTablet(getActivity());
 
         // Dont display the lock clock preference if its not installed
         removePreferenceIfPackageNotInstalled(findPreference(KEY_LOCK_CLOCK));
@@ -161,6 +185,28 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
         mLockscreenHideInitialPageHints.setChecked(Settings.System.getBoolean(getActivity().getContentResolver(),
                 Settings.System.LOCKSCREEN_HIDE_INITIAL_PAGE_HINTS, false));
 
+        mCustomBackground = (ListPreference) findPreference(KEY_BACKGROUND_PREF);
+        mCustomBackground.setOnPreferenceChangeListener(this);
+        wallpaperImage = new File(mActivity.getFilesDir()+"/lockwallpaper");
+        wallpaperTemporary = new File(mActivity.getCacheDir()+"/lockwallpaper.tmp");
+
+        float bgAlpha;
+        try{
+            bgAlpha = Settings.System.getFloat(getActivity()
+                .getContentResolver(),
+                Settings.System.LOCKSCREEN_ALPHA);
+         }catch (Exception e) {
+            bgAlpha = 0;
+            bgAlpha = Settings.System.getFloat(getActivity()
+                .getContentResolver(),
+                Settings.System.LOCKSCREEN_ALPHA, 0.0f);
+         }
+        mBgAlpha = (SeekBarPreference) findPreference(KEY_BACKGROUND_ALPHA_PREF);
+        mBgAlpha.setInitValue((int) (bgAlpha * 100));
+        mBgAlpha.setProperty(Settings.System.LOCKSCREEN_ALPHA);
+        mBgAlpha.setOnPreferenceChangeListener(this);
+
+        updateCustomBackgroundSummary();
         setHasOptionsMenu(true);
     }
 
@@ -253,10 +299,109 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
         boolean handled = false;
-        if (preference == mLockscreenTextColor) {
-            String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String.valueOf(newValue)));
+        if (preference == mCustomBackground) {
+            int indexOf = mCustomBackground.findIndexOfValue(objValue.toString());
+            switch (indexOf) {
+            //Displays color dialog when user has chosen color fill
+            case 0:
+                final ColorPickerView colorView = new ColorPickerView(mActivity);
+                int currentColor = Settings.System.getInt(getContentResolver(),
+                        Settings.System.LOCKSCREEN_BACKGROUND, -1);
+                if (currentColor != -1) {
+                    colorView.setColor(currentColor);
+                }
+                colorView.setAlphaSliderVisible(false);
+                new AlertDialog.Builder(mActivity)
+                .setTitle(R.string.lockscreen_custom_background_dialog_title)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_BACKGROUND, colorView.getColor());
+                        Settings.System.putInt(getContentResolver(),
+                                Settings.System.LOCKSCREEN_BACKGROUND_VALUE, 0);
+                        updateCustomBackgroundSummary();
+                    }
+                }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).setView(colorView).show();
+                return false;
+            //Launches intent for user to select an image/crop it to set as background
+            case 1:
+                Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                intent.putExtra("crop", "true");
+                intent.putExtra("scale", true);
+                intent.putExtra("scaleUpIfNeeded", false);
+                intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
+                Display display = mActivity.getWindowManager().getDefaultDisplay();
+                int width = display.getWidth();
+                int height = display.getHeight();
+                Rect rect = new Rect();
+                Window window = mActivity.getWindow();
+                window.getDecorView().getWindowVisibleDisplayFrame(rect);
+                int statusBarHeight = rect.top;
+                int contentViewTop = window.findViewById(Window.ID_ANDROID_CONTENT).getTop();
+                int titleBarHeight = contentViewTop - statusBarHeight;
+                // Lock screen for tablets visible section are different in landscape/portrait,
+                // image need to be cropped correctly, like wallpaper setup for scrolling in background in home screen
+                // other wise it does not scale correctly
+                if (mIsScreenLarge) {
+                    width = mActivity.getWallpaperDesiredMinimumWidth();
+                    height = mActivity.getWallpaperDesiredMinimumHeight();
+                    float spotlightX = (float) display.getWidth() / width;
+                    float spotlightY = (float) display.getHeight() / height;
+                    intent.putExtra("aspectX", width);
+                    intent.putExtra("aspectY", height);
+                    intent.putExtra("outputX", width);
+                    intent.putExtra("outputY", height);
+                    intent.putExtra("spotlightX", spotlightX);
+                    intent.putExtra("spotlightY", spotlightY);
+
+                } else {
+                    boolean isPortrait = getResources().getConfiguration().orientation ==
+                        Configuration.ORIENTATION_PORTRAIT;
+                    intent.putExtra("aspectX", isPortrait ? width : height - titleBarHeight);
+                    intent.putExtra("aspectY", isPortrait ? height - titleBarHeight : width);
+                }
+                try {
+                    wallpaperTemporary.createNewFile();
+                    wallpaperTemporary.setWritable(true, false);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(wallpaperTemporary));
+                    intent.putExtra("return-data", false);
+                    mActivity.startActivityFromFragment(this, intent, LOCKSCREEN_BACKGROUND);
+                } catch (IOException e) {
+                } catch (ActivityNotFoundException e) {
+                }
+                return false;
+            //Sets background color to default
+            case 2:
+                Settings.System.putString(getContentResolver(),
+                        Settings.System.LOCKSCREEN_BACKGROUND, null);
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.LOCKSCREEN_BACKGROUND_VALUE, indexOf);
+                updateCustomBackgroundSummary();
+                return false;
+            case 3:
+                Settings.System.putString(getContentResolver(),
+                        Settings.System.LOCKSCREEN_BACKGROUND, null);
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.LOCKSCREEN_BACKGROUND_VALUE, indexOf);
+                updateCustomBackgroundSummary();
+                break;
+            }
+            return true;
+        } else if (preference == mBgAlpha) {
+            float val = Float.parseFloat((String) objValue);
+            Settings.System.putFloat(getActivity().getContentResolver(),
+                    Settings.System.LOCKSCREEN_ALPHA, val / 100);
+            return true;
+        } else if (preference == mLockscreenTextColor) {
+            String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String.valueOf(objValue)));
             preference.setSummary(hex);
             int intHex = ColorPickerPreference.convertToColorInt(hex);
             Settings.System.putInt(getActivity().getContentResolver(),
@@ -264,6 +409,72 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
             return true;
         }
         return false;
+    }
+
+    private void updateCustomBackgroundSummary() {
+        int resId;
+            int customBackground = Settings.System.getInt(mResolver,
+                    Settings.System.LOCKSCREEN_BACKGROUND_VALUE, 3);
+        if (customBackground == 3) {
+            resId = R.string.lockscreen_background_default_wallpaper;
+            mCustomBackground.setValueIndex(3);
+            mBgAlpha.setEnabled(false);
+        } else if (customBackground == 2) {
+            resId = R.string.lockscreen_background_full_transparent;
+            mCustomBackground.setValueIndex(2);
+            mBgAlpha.setEnabled(false);
+        } else if (customBackground == 1) {
+            resId = R.string.lockscreen_background_custom_image;
+            mCustomBackground.setValueIndex(1);
+            mBgAlpha.setEnabled(true);
+        } else {
+            resId = R.string.lockscreen_background_color_fill;
+            mCustomBackground.setValueIndex(0);
+            mBgAlpha.setEnabled(true);
+        }
+        mCustomBackground.setSummary(getResources().getString(resId));
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == LOCKSCREEN_BACKGROUND) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (wallpaperTemporary.exists()) {
+                    wallpaperTemporary.renameTo(wallpaperImage);
+                }
+                wallpaperImage.setReadOnly();
+                Toast.makeText(mActivity, getResources().getString(R.string.
+                        lockscreen_background_result_successful), Toast.LENGTH_LONG).show();
+                Settings.System.putString(getContentResolver(),
+                        Settings.System.LOCKSCREEN_BACKGROUND,"");
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.LOCKSCREEN_BACKGROUND_VALUE, 1);
+                updateCustomBackgroundSummary();
+            } else {
+                if (wallpaperTemporary.exists()) {
+                    wallpaperTemporary.delete();
+                }
+                Toast.makeText(mActivity, getResources().getString(R.string.
+                        lockscreen_background_result_not_successful), Toast.LENGTH_LONG).show();
+            }
+        }
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_PICK_WALLPAPER) {
+
+                FileOutputStream wallpaperStream = null;
+                try {
+                    wallpaperStream = mContext.openFileOutput(WALLPAPER_NAME,
+                            Context.MODE_WORLD_READABLE);
+                } catch (FileNotFoundException e) {
+                    return; // NOOOOO
+                }
+
+                Uri selectedImageUri = getLockscreenExternalUri();
+                Bitmap bitmap = BitmapFactory.decodeFile(selectedImageUri.getPath());
+
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, wallpaperStream);
+            }
+        }
     }
 
     @Override
@@ -291,26 +502,6 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
         File wallpaper = new File(dir, WALLPAPER_NAME);
 
         return Uri.fromFile(wallpaper);
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == REQUEST_PICK_WALLPAPER) {
-
-                FileOutputStream wallpaperStream = null;
-                try {
-                    wallpaperStream = mContext.openFileOutput(WALLPAPER_NAME,
-                            Context.MODE_WORLD_READABLE);
-                } catch (FileNotFoundException e) {
-                    return; // NOOOOO
-                }
-
-                Uri selectedImageUri = getLockscreenExternalUri();
-                Bitmap bitmap = BitmapFactory.decodeFile(selectedImageUri.getPath());
-
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, wallpaperStream);
-            }
-        }
     }
 
     public void copy(File src, File dst) throws IOException {
@@ -345,4 +536,3 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
         return false;
     }
 }
-
