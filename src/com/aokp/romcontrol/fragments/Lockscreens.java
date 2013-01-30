2b14fb7c6f205fac4126f7b87fde0d12a0e622cd
burstlam <burstlam@gmail.com>
2013-01-30 22:28:16 +0800

    Revert "Port Lockscreen Custom Image (2/2)"
    This reverts commit c8d8a666464507ada1409cfb33f9dc19cc5644f6.
    
    Conflicts:
    	res/values/arrays.xml
    	res/values/strings.xml
    	src/com/aokp/romcontrol/fragments/Lockscreens.java
    
    Change-Id: I6f6c418c9ef07972341320ebf2993ea22b4256dc

--- src/com/aokp/romcontrol/fragments/Lockscreens.java
+++ src/com/aokp/romcontrol/fragments/Lockscreens.java
@@ -81,12 +81,6 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
     private static final String KEY_LOCKSCREEN_MAXIMIZE_WIDGETS = "lockscreen_maximize_widgets";
     private static final String PREF_LOCKSCREEN_LONGPRESS_CHALLENGE = "lockscreen_longpress_challenge";
 
-    private static final int REQUEST_CODE_BG_WALLPAPER = 1024;
-    private static final int LOCKSCREEN_BACKGROUND_COLOR_FILL = 0;
-    private static final int LOCKSCREEN_BACKGROUND_CUSTOM_IMAGE = 1;
-    private static final int LOCKSCREEN_BACKGROUND_DEFAULT_WALLPAPER = 2;
-    private static final String KEY_BACKGROUND_PREF = "lockscreen_background";
-
     Preference mLockscreenTargets;
 
     CheckBoxPreference mVolumeRockerWake;
@@ -99,12 +93,8 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
     CheckBoxPreference mLockscreenHideInitialPageHints;
     CheckBoxPreference mMaximizeWidgets;
     CheckBoxPreference mLockscreenUseCarousel;
-    ListPreference mCustomBackground;
     CheckBoxPreference mLockscreenLongpressChallenge;
     CheckBoxPreference mCameraWidget;
-
-    private File mWallpaperImage;
-    private File mWallpaperTemporary;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -160,13 +150,6 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
         mLockscreenUseCarousel.setChecked(Settings.System.getBoolean(getActivity().getContentResolver(),
                 Settings.System.LOCKSCREEN_USE_WIDGET_CONTAINER_CAROUSEL, false));
 
-        mCustomBackground = (ListPreference) findPreference(KEY_BACKGROUND_PREF);
-        mCustomBackground.setOnPreferenceChangeListener(this);
-        updateCustomBackgroundSummary();
-
-        mWallpaperImage = new File(getActivity().getFilesDir()+"/lockwallpaper");
-        mWallpaperTemporary = new File(getActivity().getCacheDir()+"/lockwallpaper.tmp");
-
         mLockscreenLongpressChallenge = (CheckBoxPreference)findPreference(PREF_LOCKSCREEN_LONGPRESS_CHALLENGE);
         mLockscreenLongpressChallenge.setChecked(Settings.System.getBoolean(getActivity().getContentResolver(),
                 Settings.System.LOCKSCREEN_LONGPRESS_CHALLENGE, false));
@@ -177,23 +160,6 @@ PREF_LOCKSCREEN_LONGPRESS_CHALLENGE));
         }
 
         setHasOptionsMenu(true);
-    }
-
-    private void updateCustomBackgroundSummary() {
-        int resId;
-        String value = Settings.System.getString(getContentResolver(),
-                Settings.System.LOCKSCREEN_BACKGROUND);
-        if (value == null) {
-            resId = R.string.lockscreen_background_default_wallpaper;
-            mCustomBackground.setValueIndex(LOCKSCREEN_BACKGROUND_DEFAULT_WALLPAPER);
-        } else if (value.isEmpty()) {
-            resId = R.string.lockscreen_background_custom_image;
-            mCustomBackground.setValueIndex(LOCKSCREEN_BACKGROUND_CUSTOM_IMAGE);
-        } else {
-            resId = R.string.lockscreen_background_color_fill;
-            mCustomBackground.setValueIndex(LOCKSCREEN_BACKGROUND_COLOR_FILL);
-        }
-        mCustomBackground.setSummary(getResources().getString(resId));
     }
 
     @Override
@@ -260,31 +226,6 @@ PREF_LOCKSCREEN_LONGPRESS_CHALLENGE));
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE_BG_WALLPAPER) {
-            int hintId;
-
-            if (resultCode == Activity.RESULT_OK) {
-                if (mWallpaperTemporary.exists()) {
-                    mWallpaperTemporary.renameTo(mWallpaperImage);
-                }
-                mWallpaperImage.setReadOnly();
-                hintId = R.string.lockscreen_background_result_successful;
-                Settings.System.putString(getContentResolver(),
-                        Settings.System.LOCKSCREEN_BACKGROUND, "");
-                updateCustomBackgroundSummary();
-            } else {
-                if (mWallpaperTemporary.exists()) {
-                    mWallpaperTemporary.delete();
-                }
-                hintId = R.string.lockscreen_background_result_not_successful;
-            }
-            Toast.makeText(getActivity(),
-                    getResources().getString(hintId), Toast.LENGTH_LONG).show();
-        }
-    }
-
-    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         boolean handled = false;
         if (preference == mLockscreenTextColor) {
@@ -299,9 +240,6 @@ PREF_LOCKSCREEN_LONGPRESS_CHALLENGE));
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.LOCKSCREEN_MAXIMIZE_WIDGETS, value ? 1 : 0);
             return true;
-        } else if (preference == mCustomBackground) {
-            int selection = mCustomBackground.findIndexOfValue(newValue.toString());
-            return handleBackgroundSelection(selection);
         }
         return false;
     }
@@ -327,81 +265,6 @@ PREF_LOCKSCREEN_LONGPRESS_CHALLENGE));
                 return true;
             }
         }
-        return false;
-    }
-
-    private boolean handleBackgroundSelection(int selection) {
-        if (selection == LOCKSCREEN_BACKGROUND_COLOR_FILL) {
-            final ColorPickerView colorView = new ColorPickerView(getActivity());
-            int currentColor = Settings.System.getInt(getContentResolver(),
-                    Settings.System.LOCKSCREEN_BACKGROUND, -1);
-
-            if (currentColor != -1) {
-                colorView.setColor(currentColor);
-            }
-            colorView.setAlphaSliderVisible(true);
-
-            new AlertDialog.Builder(getActivity())
-                    .setTitle(R.string.lockscreen_custom_background_dialog_title)
-                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Settings.System.putInt(getContentResolver(),
-                                    Settings.System.LOCKSCREEN_BACKGROUND, colorView.getColor());
-                            updateCustomBackgroundSummary();
-                        }
-                    })
-                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    })
-                    .setView(colorView)
-                    .show();
-        } else if (selection == LOCKSCREEN_BACKGROUND_CUSTOM_IMAGE) {
-            final Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
-            intent.setType("image/*");
-            intent.putExtra("crop", "true");
-            intent.putExtra("scale", true);
-            intent.putExtra("scaleUpIfNeeded", false);
-            intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
-
-            final Display display = getActivity().getWindowManager().getDefaultDisplay();
-            final Rect rect = new Rect();
-            final Window window = getActivity().getWindow();
-
-            window.getDecorView().getWindowVisibleDisplayFrame(rect);
-
-            int statusBarHeight = rect.top;
-            int contentViewTop = window.findViewById(Window.ID_ANDROID_CONTENT).getTop();
-            int titleBarHeight = contentViewTop - statusBarHeight;
-            boolean isPortrait = getResources().getConfiguration().orientation ==
-                    Configuration.ORIENTATION_PORTRAIT;
-
-            int width = display.getWidth();
-            int height = display.getHeight() - titleBarHeight;
-
-            intent.putExtra("aspectX", isPortrait ? width : height);
-            intent.putExtra("aspectY", isPortrait ? height : width);
-
-            try {
-                mWallpaperTemporary.createNewFile();
-                mWallpaperTemporary.setWritable(true, false);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT ,Uri.fromFile(mWallpaperTemporary));
-                intent.putExtra("return-data", false);
-                getActivity().startActivityFromFragment(this, intent, REQUEST_CODE_BG_WALLPAPER);
-                //Ignored would be preferable to nothing
-            } catch (IOException e) {
-            } catch (ActivityNotFoundException e) {
-            }
-        } else if (selection == LOCKSCREEN_BACKGROUND_DEFAULT_WALLPAPER) {
-            Settings.System.putString(getContentResolver(),
-                    Settings.System.LOCKSCREEN_BACKGROUND, null);
-            updateCustomBackgroundSummary();
-            return true;
-        }
-
         return false;
     }
 }
