5e7862d71066cfaefce837ddf7ede81ea160cea1
Zaphod <mwilson@acesappraisal.com>
2013-03-17 18:34:13 -0400

    RC : Allow user selectable StatusBar FontSize
    PS 1: Combined the the FW side, this will allow the user to select
    a custom font-size for text used on the StatusBar.
    Note that I put this in 'Signal' area for now, but really needs to be on the
    main preference screen as a sub-category of StatusBar (or in Gen UI?)
    I didn't want to try and put in main area as no other preferences are set
    directly there (all lead to other fragments).  But putting it in Signal
    is misleading, as it effects other areas.  Putting in GenUI might be best,
    but that area is already way too crowded.
    
    ps 2:  Rebased and moved setting to GenUI.
    ps 3: Fixed persistence of setting
    Change-Id: Iadcecf66c23d98fca5557047eedf711e47b042dd
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>
    
    Conflicts:
    	res/values/strings.xml
    	src/com/aokp/romcontrol/fragments/UserInterface.java

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -120,6 +120,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     private static final CharSequence PREF_MISC = "misc";
     private static final CharSequence PREF_DISPLAY = "display";
 
+    private static int STOCK_FONT_SIZE = 16;
     private static final int REQUEST_PICK_WALLPAPER = 201;
     // private static final int REQUEST_PICK_CUSTOM_ICON = 202; // unused
     private static final int REQUEST_PICK_BOOT_ANIMATION = 203;
@@ -150,7 +151,8 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     ListPreference mCrtMode;
     ListPreference mLowBatteryWarning;
     ListPreference mNotificationsBehavior;
-    ListPreference mStatusBarIconOpacity; 
+    ListPreference mStatusBarIconOpacity;
+    ListPreference mFontsize;
 
     private StatusBarBrightnessChangedObserver mStatusBarBrightnessChangedObserver;
 
@@ -305,6 +307,11 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
                 Settings.System.STATUS_BAR_NOTIF_ICON_OPACITY, 140);
         mStatusBarIconOpacity.setValue(String.valueOf(iconOpacity));
         mStatusBarIconOpacity.setOnPreferenceChangeListener(this);
+
+        mFontsize = (ListPreference) findPreference("status_bar_fontsize");
+        mFontsize.setOnPreferenceChangeListener(this);
+        mFontsize.setValue(Integer.toString(Settings.System.getInt(mContentRes,
+                Settings.System.STATUSBAR_FONT_SIZE, STOCK_FONT_SIZE)));
 
         if (isTablet(mContext)) {
             mStatusBarAutoHide.setEnabled(false);
@@ -678,7 +685,13 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
             int iconOpacity = Integer.valueOf((String) newValue);
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.STATUS_BAR_NOTIF_ICON_OPACITY, iconOpacity);
-            return true; 
+            return true;
+        } else if (preference == mFontsize) {
+			int val = Integer.parseInt((String) newValue);
+			Settings.System.putInt(mContentRes,
+			Settings.System.STATUSBAR_FONT_SIZE, val);
+			Helpers.restartSystemUI();
+			return true;
         }
         return false;
     }
