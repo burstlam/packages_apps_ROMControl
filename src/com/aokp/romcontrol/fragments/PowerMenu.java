128dffa6d2a3912903151ab9e0c660d4c8f57568
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-04-14 12:33:43 +0200

    RC: Powermenu styling update - based on trello
    PS2: Adjust Preference behavior too
    
    Change-Id: Ie65d747c20ccc5c3ae0db6a01e9318f1d2646d52
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>
    
    Conflicts:
    	res/values/strings.xml
    	res/xml/prefs_powermenu.xml
    	src/com/aokp/romcontrol/fragments/PowerMenu.java

--- src/com/aokp/romcontrol/fragments/PowerMenu.java
+++ src/com/aokp/romcontrol/fragments/PowerMenu.java
@@ -3,11 +3,11 @@ package com.aokp.romcontrol.fragments;
 
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
+import android.preference.SwitchPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 
@@ -30,17 +30,17 @@ public class PowerMenu extends AOKPPreferenceFragment implements
     private static final String PREF_VOLUME_STATE_TOGGLE = "show_volume_state_toggle";
     private static final String PREF_REBOOT_KEYGUARD = "show_reboot_keyguard";
 
-    //CheckBoxPreference mShowPowerSaver;
-    CheckBoxPreference mShowPowerOff;
-    CheckBoxPreference mShowScreenShot;
-    CheckBoxPreference mShowTorchToggle;
-    CheckBoxPreference mShowAirplaneToggle;
-    CheckBoxPreference mShowNavBarHide;
-    CheckBoxPreference mExpandedDesktopPref;
+    //SwitchPreference mShowPowerSaver;
+    SwitchPreference mShowPowerOff;
+    SwitchPreference mShowScreenShot;
+    SwitchPreference mShowTorchToggle;
+    SwitchPreference mShowAirplaneToggle;
+    SwitchPreference mShowNavBarHide;
+    SwitchPreference mExpandedDesktopPref;
     ListPreference mExpandedDesktopSbPref;
-    private CheckBoxPreference mPieRestart;
-    CheckBoxPreference mShowVolumeStateToggle;
-    CheckBoxPreference mShowRebootKeyguard;
+    private SwitchPreference mPieRestart;
+    SwitchPreference mShowVolumeStateToggle;
+    SwitchPreference mShowRebootKeyguard;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,7 +49,7 @@ public class PowerMenu extends AOKPPreferenceFragment implements
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.prefs_powermenu);
         /*
-        mShowPowerSaver = (CheckBoxPreference) findPreference(PREF_POWER_SAVER);
+        mShowPowerSaver = (SwitchPreference) findPreference(PREF_POWER_SAVER);
         int powerSaverVal = 0;
         try {
             powerSaverVal = Settings.Secure.getInt(getActivity()
@@ -59,122 +59,125 @@ public class PowerMenu extends AOKPPreferenceFragment implements
             mShowPowerSaver
                     .setSummary("You need to enable power saver before you can see it in the power menu.");
         }
-        mShowPowerSaver.setChecked(powerSaverVal != 0); */
+        mShowPowerSaver.setChecked(powerSaverVal != 0);
+        mShowPowerSaver.setOnPreferenceChangeListener(this); */
 
-        mShowPowerOff = (CheckBoxPreference) findPreference(PREF_POWER_OFF);
-        mShowPowerOff.setChecked(Settings.System.getInt(getActivity()
-                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_POWER_OFF, 1) == 1);
+        mShowPowerOff = (SwitchPreference) findPreference(PREF_POWER_OFF);
+        mShowPowerOff.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.POWER_DIALOG_SHOW_POWER_OFF, true));
+        mShowPowerOff.setOnPreferenceChangeListener(this);
 
-        mShowTorchToggle = (CheckBoxPreference) findPreference(PREF_TORCH_TOGGLE);
-        mShowTorchToggle.setChecked(Settings.System.getBoolean(getActivity()
-                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE, false));
+        mShowTorchToggle = (SwitchPreference) findPreference(PREF_TORCH_TOGGLE);
+        mShowTorchToggle.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE, false));
+        mShowTorchToggle.setOnPreferenceChangeListener(this);
 
-        mShowScreenShot = (CheckBoxPreference) findPreference(PREF_SCREENSHOT);
-        mShowScreenShot.setChecked(Settings.System.getBoolean(getActivity()
-                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_SCREENSHOT, false));
+        mShowScreenShot = (SwitchPreference) findPreference(PREF_SCREENSHOT);
+        mShowScreenShot.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.POWER_DIALOG_SHOW_SCREENSHOT, false));
+        mShowScreenShot.setOnPreferenceChangeListener(this);
 
-        mShowAirplaneToggle = (CheckBoxPreference) findPreference(PREF_AIRPLANE_TOGGLE);
-        mShowAirplaneToggle.setChecked(Settings.System.getBoolean(getActivity()
-                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_AIRPLANE_TOGGLE, true));
+        mShowAirplaneToggle = (SwitchPreference) findPreference(PREF_AIRPLANE_TOGGLE);
+        mShowAirplaneToggle.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.POWER_DIALOG_SHOW_AIRPLANE_TOGGLE, true));
+        mShowAirplaneToggle.setOnPreferenceChangeListener(this);
 
-        mShowNavBarHide = (CheckBoxPreference) findPreference(PREF_NAVBAR_HIDE);
-        mShowNavBarHide.setChecked(Settings.System.getBoolean(getActivity()
-                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE, true));
+        mShowNavBarHide = (SwitchPreference) findPreference(PREF_NAVBAR_HIDE);
+        mShowNavBarHide.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE, true));
+        mShowNavBarHide.setOnPreferenceChangeListener(this);
 
-        mShowVolumeStateToggle = (CheckBoxPreference) findPreference(PREF_VOLUME_STATE_TOGGLE);
+        mShowVolumeStateToggle = (SwitchPreference) findPreference(PREF_VOLUME_STATE_TOGGLE);
         mShowVolumeStateToggle.setChecked(Settings.System.getBoolean(mContentRes,
                 Settings.System.POWER_DIALOG_SHOW_VOLUME_STATE_TOGGLE, true));
+        mShowVolumeStateToggle.setOnPreferenceChangeListener(this);
 
-        mExpandedDesktopPref = (CheckBoxPreference) findPreference(PREF_EXPANDED_DESKTOP_TOGGLE);
-        mExpandedDesktopPref.setChecked(Settings.System.getInt(getActivity()
-                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_EXPANDED_DESKTOP_TOGGLE, 
-                0) == 1);
+        mExpandedDesktopPref = (SwitchPreference) findPreference(PREF_EXPANDED_DESKTOP_TOGGLE);
+        mExpandedDesktopPref.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.POWER_DIALOG_SHOW_EXPANDED_DESKTOP_TOGGLE, false));
+        mExpandedDesktopPref.setOnPreferenceChangeListener(this);
 
         PreferenceScreen prefSet = getPreferenceScreen();
         mExpandedDesktopSbPref = (ListPreference) prefSet.findPreference(EXPANDED_DESKTOP_STYLE);
         mExpandedDesktopSbPref.setOnPreferenceChangeListener(this);
-        int expandedDesktopValue = Settings.System.getInt(getContentResolver(), Settings.System.EXPANDED_DESKTOP_STYLE, 0);
+        int expandedDesktopValue = Settings.System.getInt(mContentRes,
+                Settings.System.EXPANDED_DESKTOP_STYLE, 0);
         mExpandedDesktopSbPref.setValue(String.valueOf(expandedDesktopValue));
         updateExpandedDesktopSummary(expandedDesktopValue);
 
-        mPieRestart = (CheckBoxPreference) prefSet.findPreference(PIE_RESTART);
-        mPieRestart.setChecked(Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.EXPANDED_DESKTOP_RESTART_LAUNCHER, 0) == 1);
+        mPieRestart = (SwitchPreference) prefSet.findPreference(PIE_RESTART);
+        mPieRestart.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.EXPANDED_DESKTOP_RESTART_LAUNCHER, false));
+        mPieRestart.setOnPreferenceChangeListener(this);
 
-        mShowRebootKeyguard = (CheckBoxPreference) findPreference(PREF_REBOOT_KEYGUARD);
-        mShowRebootKeyguard.setChecked(Settings.System.getBoolean(getActivity()
-                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_REBOOT_KEYGUARD, true));
+        mShowRebootKeyguard = (SwitchPreference) findPreference(PREF_REBOOT_KEYGUARD);
+        mShowRebootKeyguard.setChecked(Settings.System.getBoolean(mContentRes,
+                Settings.System.POWER_DIALOG_SHOW_REBOOT_KEYGUARD, true));
+        mShowRebootKeyguard.setOnPreferenceChangeListener(this);
 
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mExpandedDesktopSbPref) {
-            int expandedDesktopValue = Integer.valueOf((String) newValue);
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        if (preference == mShowScreenShot) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_SCREENSHOT,
+                     (Boolean) value);
+            return true;
+        /*
+        } else if (preference == mShowPowerSaver) {
+            Settings.System.putInt(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_POWER_SAVER,
+                     (Boolean) value ? 1 : 0 );
+            return true; */
+        } else if (preference == mShowPowerOff) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_POWER_OFF,
+                    (Boolean) value);
+            return true;
+        } else if (preference == mShowTorchToggle) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE,
+                    (Boolean) value);
+            return true;
+        } else if (preference == mShowAirplaneToggle) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_AIRPLANE_TOGGLE,
+                    (Boolean) value);
+            return true;
+        } else if (preference == mShowNavBarHide) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE,
+                    (Boolean) value);
+            return true;
+        } else if (preference == mShowVolumeStateToggle) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_VOLUME_STATE_TOGGLE,
+                    (Boolean) value);
+            return true;
+        } else if (preference == mExpandedDesktopPref) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_EXPANDED_DESKTOP_TOGGLE,
+                    (Boolean) value);
+            return true;
+        } else if (preference == mPieRestart) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.EXPANDED_DESKTOP_RESTART_LAUNCHER, 
+                    (Boolean) value);
+            return true;
+        } else if (preference == mShowRebootKeyguard) {
+            Settings.System.putBoolean(mContentRes,
+                    Settings.System.POWER_DIALOG_SHOW_REBOOT_KEYGUARD,
+                    (Boolean) value);
+            return true;
+        } else if (preference == mExpandedDesktopSbPref) {
+            int expandedDesktopValue = Integer.valueOf((String) value);
             Settings.System.putInt(getContentResolver(),
                     Settings.System.EXPANDED_DESKTOP_STYLE, expandedDesktopValue);
             updateExpandedDesktopSummary(expandedDesktopValue);
             return true;
         }
         return false;
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mShowScreenShot) {
-            Settings.System.putBoolean(mContentRes,
-                    Settings.System.POWER_DIALOG_SHOW_SCREENSHOT,
-                    ((CheckBoxPreference)preference).isChecked());
-            return true;
-        /*
-        } else if (preference == mShowPowerSaver) {
-            Settings.System.putInt(mContentRes,
-                    Settings.System.POWER_DIALOG_SHOW_POWER_SAVER,
-                    ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
-            return true; */
-        } else if (preference == mShowPowerOff) {
-            Settings.System.putInt(mContentRes,
-                    Settings.System.POWER_DIALOG_SHOW_POWER_OFF,
-                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
-            return true;
-        } else if (preference == mShowTorchToggle) {
-            Settings.System.putBoolean(mContentRes,
-                    Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE,
-                    ((CheckBoxPreference)preference).isChecked());
-            return true;
-        } else if (preference == mShowAirplaneToggle) {
-            Settings.System.putBoolean(mContentRes,
-                    Settings.System.POWER_DIALOG_SHOW_AIRPLANE_TOGGLE,
-                    ((CheckBoxPreference)preference).isChecked());
-            return true;
-        } else if (preference == mShowNavBarHide) {
-            Settings.System.putBoolean(mContentRes,
-                    Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE,
-                    ((CheckBoxPreference)preference).isChecked());
-            return true;
-        } else if (preference == mShowVolumeStateToggle) {
-            Settings.System.putBoolean(mContentRes,
-                    Settings.System.POWER_DIALOG_SHOW_VOLUME_STATE_TOGGLE,
-                    ((CheckBoxPreference)preference).isChecked());
-            return true;
-        } else if (preference == mExpandedDesktopPref) {
-            Settings.System.putInt(mContentRes,
-                    Settings.System.POWER_DIALOG_SHOW_EXPANDED_DESKTOP_TOGGLE,
-                    ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
-            return true;
-        } else if (preference == mPieRestart) {
-            Settings.System.putInt(mContentRes,
-                    Settings.System.EXPANDED_DESKTOP_RESTART_LAUNCHER, 
-                    ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
-            return true;
-        } else if (preference == mShowRebootKeyguard) {
-            Settings.System.putBoolean(mContentRes,
-                    Settings.System.POWER_DIALOG_SHOW_REBOOT_KEYGUARD,
-                    ((CheckBoxPreference)preference).isChecked());
-            return true;
-        }
-
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     private void updateExpandedDesktopSummary(int value) {
