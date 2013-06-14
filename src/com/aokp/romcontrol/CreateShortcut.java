2668ad9b62a1ade36c4cbb3c5c21fb3c76efd4c3
burstlam <burstlam@gmail.com>
2013-06-15 01:07:56 +0800

    Add back shortcut for Perforamance Control
    Change-Id: Ibf4b102c052d58969cfc87ec20e146e8b5077685

--- src/com/aokp/romcontrol/CreateShortcut.java
+++ src/com/aokp/romcontrol/CreateShortcut.java
@@ -76,6 +76,8 @@ public class CreateShortcut extends LauncherActivity {
             return R.drawable.ic_rom_control_general_ui;
         } else if (c.equals("Weather")) {
             return R.drawable.ic_rom_control_weather;
+        } else if (c.equals("Performance")) {
+            return R.drawable.ic_rom_control_performance;
         } else {
             return R.mipmap.ic_launcher;
         }
