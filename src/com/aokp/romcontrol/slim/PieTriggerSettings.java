8d65a8203e5ea0107b62c55dd06c6ba90566a25b
burstlam <burstlam@gmail.com>
2013-07-20 23:44:04 +0800

    Derp
    Change-Id: I19dfd22a29ddc5469d17db65a68e4379e6ca209b

--- src/com/aokp/romcontrol/slim/PieTriggerSettings.java
+++ src/com/aokp/romcontrol/slim/PieTriggerSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.aokp.romcontrol.fragments;
+package com.aokp.romcontrol.slim;
 
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
